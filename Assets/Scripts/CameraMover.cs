b4a8e03a2c09b0a87ce651cfe94c614418098052
AlexanderKravchenkoS <kravchenko.alexander.s@gmail.com>
2021-05-18 10:54:06 +0300

    Camera update

--- Assets/Scripts/CameraMover.cs
+++ Assets/Scripts/CameraMover.cs
@@ -4,16 +4,14 @@ using UnityEngine;
 
 public class CameraMover : MonoBehaviour
 {
-    private int speed = 500;
+    private float speed = 500;
 
     private void Update()
     {
         if (Input.GetMouseButton(1))
         {
             float mouseX = Input.GetAxisRaw("Mouse X");
-            float mouseY = Input.GetAxisRaw("Mouse Y");
-            Mathf.Clamp(mouseY, -90, 90);
-            transform.eulerAngles += new Vector3(mouseY, mouseX, 0) * Time.deltaTime * speed;
+            transform.localEulerAngles += new Vector3(0, mouseX, 0) * Time.deltaTime * speed;
         }
     }
 }
